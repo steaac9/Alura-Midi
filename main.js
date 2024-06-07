80c69cc3f83142273c7c15af63c009fb01981368
Stephanie <steaac9@gmail.com>
2024-06-06 22:12:36 -0600

    Click en el botón y Clap solita

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ function playSonidoPom () {
    document.querySelector ('#sonido_tecla_pom').play(); 
 }
 
-
-document.querySelector ('.tecla_pom');
+document.querySelector ('.tecla_pom').onclick = playSonidoPom;
 
 function playSonidoClap () {
     document.querySelector ('#sonido_tecla_clap').play();    
 }
+
+document.querySelector ('.tecla_clap').onclick = playSonidoClap;
