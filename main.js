a74b87ecb9e7fe252858d440fa991d7aa25df435
Stephanie <steaac9@gmail.com>
2024-06-17 20:44:51 -0600

    funciones anon

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function playSonidoPom (){
-    document.querySelector('#sonido_tecla_pom').play();
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,7 +7,9 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0
 
 while (contador < 9) {
-   listaDeTeclas[contador].onclick = playSonidoPom;
-   contador = contador + 1
-   console.log('Vuelta' + contador)
+   listaDeTeclas[contador].onclick = function (){
+    playSonido('#sonido_tecla_tom');
+   };
+   contador = contador + 1;
+   console.log('Vuelta' + contador);
 }
