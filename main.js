27e3f559c365d0582dbc73a0e4832e2737652ba4
Stephanie <steaac9@gmail.com>
2024-06-17 19:37:56 -0600

    aula 3

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
-function playSonidoPom () {
-   document.querySelector ('#sonido_tecla_pom').play(); 
+function playSonidoPom (){
+    document.querySelector('#sonido_tecla_pom').play();
 }
 
-document.querySelector ('.tecla_pom').onclick = playSonidoPom;
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function playSonidoClap () {
-    document.querySelector ('#sonido_tecla_clap').play();    
-}
-
-document.querySelector ('.tecla_clap').onclick = playSonidoClap;
-
-function playSonidoTim () {
-   document.querySelector ('#sonido_tecla_Tim').play(); 
-}
-
-document.querySelector ('.tecla_tim').onclick = playSonidoTim;
-
-function playSonidoSplash () {
-    document.querySelector ('#sonido_tecla_splash').play();    
-}
-
-document.querySelector ('.tecla_splash').onclick = playSonidoSplash;
+listaDeTeclas[0].onclick = playSonidoPom; 
+listaDeTeclas[1].onclick = playSonidoPom; 
